7bd5c8b3531210c68bbe9336679789493b3eed5f
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-02-24 23:00:28 +0000

    surya time!
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -9,15 +9,13 @@ kernel.compiler=AOSP clang
 kernel.made=CHRISL7
 message.word=
 do.devicecheck=1
+no_magisk_check=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=spes
-device.name2=spesn
-supported.versions=11.0-14.0
-supported.patchlevels=
-supported.vendorpatchlevels=
+do.cleanuponabort=1
+device.name1=surya
+device.name2=karna
 '; } # end properties
 
 
@@ -29,8 +27,8 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=boot;
-is_slot_device=auto;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
