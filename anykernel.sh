a917dba2ffb8d9261af8dee4d9ffca7e85b9aa1c
CHRISL7 <70589286+CHRISL7@users.noreply.github.com>
2024-09-26 14:12:41 -0400

    Vauxite time

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=uvite-ksu
+kernel.string=vauxite-ksu
 kernel.compiler=AOSP clang
 kernel.made=CHRISL7
 message.word=
@@ -15,7 +15,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=spes
 device.name2=spesn
-supported.versions=11.0-14.0
+supported.versions=13.0-15.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
